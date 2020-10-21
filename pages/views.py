e073f2013a3164c5efde27de13a30e361b95a5c3
Daniel Hall <danielhall@nevada.unr.edu>
2020-10-20 19:44:45 -0700

    Added a css file for styling that is not included in boot strap. Recreated import page to follow a grid system. Structured base.html where content will split and nav bar will render on every page. Created blank homepage that extends from base.html.

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,8 @@ from django.http import HttpResponse
 def landingPageView(request):
     return render(request, 'pages/landingPage.html')
 
+def homePageView(request):
+    return render(request, 'pages/homePage.html')
+
 def importPageView(request):
     return render(request, 'pages/importPage.html')
