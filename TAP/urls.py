ea75e2163f5d0756c8a49790c0fa5ee3f566e80d
Jonathon <jhewittjrs@gmail.com>
2020-10-28 13:14:19 -0700

    Added the parser to the url list for the project

--- TAP/urls.py
+++ TAP/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('datagen/', include('datagen.urls')),
     path('admin/', admin.site.urls),
+    path('parse/', include('parsing.urls')),
     path('', include('pages.urls')),
 ]
