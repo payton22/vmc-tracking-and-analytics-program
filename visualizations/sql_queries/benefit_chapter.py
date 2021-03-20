f2ac8bea9505990db41ce9c7b303503908361577
Chris Parks <christopherparks@nevada.unr.edu>
2021-03-20 02:50:35 -0700

    Merge Payton's Changes
    Prep to combine Jon's changes

--- visualizations/sql_queries/benefit_chapter.py
+++ visualizations/sql_queries/benefit_chapter.py
@@ -1,2 +1,2 @@
-def get_query():
-    return "SELECT demographics.benefit_chapter, count(demographics.benefit_chapter) FROM visits LEFT JOIN demographics ON visits.student_id = demographics.student_id WHERE visits.check_in_date BETWEEN '%i' AND '%o' AND visits.location IN (%l) GROUP BY demographics.benefit_chapter;"
+def get_query(from_time, to_time, substr):
+    return "SELECT demographics.benefit_chapter, count(demographics.benefit_chapter) FROM visits LEFT JOIN demographics ON visits.student_id = demographics.student_id WHERE (location = \'" + substr + "\') and check_in_date >= \'" + from_time + "\' AND check_in_date <= \'" + to_time + "\' GROUP BY demographics.benefit_chapter;"
--- visualizations/sql_queries/gpa.py
+++
@@ -1,2 +0,0 @@
-def get_query():
-    return "SELECT demographics.benefit_chapter, count(demographics.benefit_chapter) FROM visits LEFT JOIN demographics ON visits.student_id = demographics.student_id WHERE visits.check_in_date BETWEEN '%i' AND '%o' AND visits.location IN (%l) GROUP BY demographics.benefit_chapter;"
