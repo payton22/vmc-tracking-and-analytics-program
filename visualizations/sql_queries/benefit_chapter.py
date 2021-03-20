14250069feea64c0826ef3607c8700912c02350e
Chris Parks <christopherparks@nevada.unr.edu>
2021-03-20 02:40:35 -0700

    Auto stash before merge of "merge" and "payton22/dashboard_test"

--- visualizations/sql_queries/benefit_chapter.py
+++ visualizations/sql_queries/benefit_chapter.py
@@ -1,2 +1,2 @@
 def get_query():
-    return "SELECT IFNULL(demographics.benefit_chapter, ''), count(demographics.benefit_chapter) FROM visits LEFT JOIN demographics ON visits.student_id = demographics.student_id GROUP BY demographics.benefit_chapter;"
+    return "SELECT demographics.benefit_chapter, count(demographics.benefit_chapter) FROM visits LEFT JOIN demographics ON visits.student_id = demographics.student_id WHERE visits.check_in_date BETWEEN '%i' AND '%o' AND visits.location IN (%l) GROUP BY demographics.benefit_chapter;"
--- visualizations/sql_queries/gpa.py
+++ visualizations/sql_queries/gpa.py
@@ -1,2 +1,2 @@
 def get_query():
-    return "select major, count(major) from demographics GROUP BY major;"
+    return "SELECT demographics.benefit_chapter, count(demographics.benefit_chapter) FROM visits LEFT JOIN demographics ON visits.student_id = demographics.student_id WHERE visits.check_in_date BETWEEN '%i' AND '%o' AND visits.location IN (%l) GROUP BY demographics.benefit_chapter;"
