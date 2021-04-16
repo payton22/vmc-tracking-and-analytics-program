210e9935ce00d2b5331543f7e031b4a4d6a500e9
Chris Parks <christopherparks@nevada.unr.edu>
2021-04-16 15:14:43 -0700

    Manual Visits Data Hooked up to DB
    Timezone is now America/Los_Angeles

--- parsing/views.py
+++ parsing/views.py
@@ -95,11 +95,140 @@ def parse_gpa(request):
     else:
         return HttpResponse("ERROR, please go to the import page and upload a file.")
 
+
 def parse_manual(request):
     if request.method == 'POST':
+        import re;
+        import datetime;
         conn = sqlite3.connect('vmc_tap.db');
+        input_dict = {}
+        
+        #Create a dictionary out of the list of items. Might already be a dictionary; rather be safe here.
+        max_record = 0;
+        for a in request.POST.items():
+            if(a[0] == 'csrfmiddlewaretoken'):
+                continue;
+            input_dict[a[0]] = a[1]
+            if(int(a[0][-1:]) > max_record):
+                max_record = int(a[0][-1:]);
+        
+        
+        #This is a list of what can be entered by the user.
+        fields_list = ['NSHEID', 'fname', 'lname', 'location', 'checkintime', 'checkouttime']
 
-        conn.close()
-    return HttpResponse(list(request.POST.items()));
-    #return redirect('importPage')
+        #This is a list of SQL variables.
+        sql_list = ['student_id', 'student_name', 'location', 'check_in_date', 'check_in_time', 'check_out_date', 'check_out_time', 'check_in_duration']
+
+        #For each record entered...
+        for i in range(0,max_record + 1):
+
+            #Is the student already in our DB?
+            #We use the NSHE ID to figure it out. They might not have entered an NSHE ID, so for now, leave it as None.
+            student_exists = None;
+
+            #Prep an array to use as a basis for forming the final SQL statement.
+            this_record = ["\'\'"] * 8;
+
+            #Now try to find fields that are filled out.
+            for index, f in enumerate(fields_list):
+                
+                #If we find a field that is filled out, store the input into a variable for easy access.
+                if(input_dict.get(f + str(i))):
+                    this_input = input_dict.get(f + str(i));
+
+                    #If they entered an NSHEID, we can query the DB to see if the student already exists.
+                    if(f == 'NSHEID'):
+                        for d in conn.execute('SELECT count(*), student_name FROM demographics WHERE student_id = ' + str(this_input) + ';'):
+                            student_exists = d[0];
+
+                            #No matter what, we include the NSHEID as a part of the query.
+                            this_record[0] = "'" + str(this_input) + "'";
+
+                            #If the student exists, we want to use their existing name, and not update it...
+                            #Unless that name is incomplete. Names with one word are not complete.
+                            #Since we only use the existing name if they enter an NSHEID, the rest of the name code
+                            #Will be dealt with later.
+                            if(d[0] > 0 and d[1].find(' ') != -1):
+                                this_record[1] = "'" + d[1] + "'";
+
+                    elif((f == 'fname' or f == 'lname') and this_record[1] == "\'\'"):
+                        #At this point, if the name is still blank, we need to assemble a new one.
+                        #If both first and name are included, use that.
+                        if(input_dict.get('fname' + str(i)) and input_dict.get('lname' + str(i))):
+                            this_record[1] = "'" + input_dict.get('fname' + str(i)) + ' ' + input_dict.get('lname' + str(i)) + "'";
+
+                        #If only one name is included, use that. If no name is provided, oh well, leave it blank.
+                        elif(input_dict.get('fname' + str(i)) and not input_dict.get('lname' + str(i))):
+                            this_record[1] = "'" + input_dict.get('fname' + str(i)) + "'";
+                        elif(not input_dict.get('fname' + str(i)) and input_dict.get('lname' + str(i))):
+                            this_record[1] = "'" + input_dict.get('fname' + str(i)) + "'";
+
+                    elif(f == 'location'):
+                        this_record[2] = "'Veteran Services_" + this_input + "'";
+
+                    elif(f == 'checkintime'):
+                        #Ensure the format is correct; if so, split it into a date and time.
+                        m = re.fullmatch('\d{2}\/\d{2}\/\d{4}T\d{2}:\d{2}',this_input);
+                        if(m):
+                            this_date = this_input[0:this_input.index('T')]
+                            this_time = this_input[this_input.index('T')+1:]
+                            this_record[3] = "'" + datetime.datetime.strptime(this_date, "%m/%d/%Y").strftime("%Y-%m-%d") + "'"
+                            this_record[4] = "'" + datetime.datetime.strptime(this_time, "%H:%M").strftime("%I:%M %p") + "'"
+                    elif(f == 'checkouttime'):
+                        m = re.fullmatch('\d{2}\/\d{2}\/\d{4}T\d{2}:\d{2}',this_input);
+                        if(m):
+                            this_date = this_input[0:this_input.index('T')]
+                            this_time = this_input[this_input.index('T')+1:]
+                            this_record[5] = "'" + datetime.datetime.strptime(this_date, "%m/%d/%Y").strftime("%Y-%m-%d") + "'"
+                            this_record[6] = "'" + datetime.datetime.strptime(this_time, "%H:%M").strftime("%I:%M %p") + "'"
+
+                    #this_record[index] = f;
+                else:
+                    continue;
+
+            #Check if the check in and out dates are both filled out; if so, we can construct a check in duration
+            if(this_record[3] != "\'\'" and this_record[5] != "\'\'"):
+                #Construct datetime objects to do a time difference
+                chk_in = datetime.datetime.strptime(this_record[3].replace("'","") + ' ' + this_record[4].replace("'",""), "%Y-%m-%d %I:%M %p")
+                chk_out = datetime.datetime.strptime(this_record[5].replace("'","") + ' ' + this_record[6].replace("'",""), "%Y-%m-%d %I:%M %p")
+
+            elif(this_record[3] == "\'\'" and this_record[5] != "\'\'"):
+                #We assume the check-in date is the same as the check out date
+                chk_in = datetime.datetime.strptime(this_record[5].replace("'","") + ' ' + this_record[6].replace("'",""), "%Y-%m-%d %I:%M %p")
+                chk_out = datetime.datetime.strptime(this_record[5].replace("'","") + ' ' + this_record[6].replace("'",""), "%Y-%m-%d %I:%M %p")
+
+                this_record[3] = "'" + chk_out.strftime("%Y-%m-%d") + "'"
+                this_record[4] = "'" + chk_out.strftime("%I:%M %p") + "'"
+
+            elif(this_record[3] != "\'\'" and this_record[5] == "\'\'"):
+                #We assume the check-out date is the same as the check in date
+                chk_in = datetime.datetime.strptime(this_record[3].replace("'","") + ' ' + this_record[4].replace("'",""), "%Y-%m-%d %I:%M %p")
+                chk_out = datetime.datetime.strptime(this_record[3].replace("'","") + ' ' + this_record[4].replace("'",""), "%Y-%m-%d %I:%M %p")
+
+                this_record[5] = "'" + chk_in.strftime("%Y-%m-%d") + "'"
+                this_record[6] = "'" + chk_in.strftime("%I:%M %p") + "'"
+
+            else:
+                #We set the check in and check out times to the current time
+                chk_in = datetime.datetime.now()
+                chk_out = datetime.datetime.now()
+
+                this_record[3] = "'" + chk_out.strftime("%Y-%m-%d") + "'"
+                this_record[4] = "'" + chk_out.strftime("%I:%M %p") + "'"
+                this_record[5] = "'" + chk_in.strftime("%Y-%m-%d") + "'"
+                this_record[6] = "'" + chk_in.strftime("%I:%M %p") + "'"
+
+            this_record[7] = "'" + str(max(round((chk_out - chk_in).total_seconds() / 60.0,2),0.0)) + "'"
+            #Record built, add to DB
+            sql_statement = 'INSERT INTO visits (';
+            sql_statement += ', '.join(sql_list);
+            sql_statement += ') VALUES (';
+            sql_statement += ', '.join(this_record);
+            sql_statement += ');';
+            conn.execute(sql_statement);
+
+        conn.commit();
+        conn.close();
+    #return HttpResponse(input_dict);
+    return redirect('importPage')
 
